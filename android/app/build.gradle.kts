11bf15374f22f90b0c01fbd81d49a483d9ad3d5c
Rafael Aguiar <rafael.henrique.aguiar@outlook.com.br>
2026-08-10 18:01:43 -0300

    build gradle ajustment

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.armazenamento_de_dados"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
