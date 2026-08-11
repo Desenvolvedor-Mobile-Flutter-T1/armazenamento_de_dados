c23cc66cb2418e4b09cda96ee4aac1e2fe956865
Rafael Aguiar <rafael.henrique.aguiar@outlook.com.br>
2026-08-10 21:55:33 -0300

    exercicio

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -11,7 +16,32 @@ class HomePage extends StatelessWidget {
         spacing: 50,
         mainAxisAlignment: .center,
         crossAxisAlignment: .stretch,
-        children: [],
+        children: [
+          ElevatedButton(
+            onPressed: () async {
+              Dio dio = Dio();
+              final result = await dio.post(
+                'https://dummyjson.com/auth/login',
+                data: {"username": "emilys", "password": "emilyspass"},
+              );
+              print('#### API: $result');
+              final secure = FlutterSecureStorage();
+              await secure.write(
+                key: 'authData',
+                value: json.encode(result.data),
+              );
+            },
+            child: Text('Consumir e salvar dados'),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              final secure = FlutterSecureStorage();
+              final result = await secure.read(key: 'authData');
+              log(json.decode(result!).toString());
+            },
+            child: Text('ler dados'),
+          ),
+        ],
       ),
     );
   }
